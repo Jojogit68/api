accb69cc08b94bdd94ff2a04294b9bec0b25d575
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-24 18:41:45 +0200

    update const port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const sequelize = require("./src/db/sequelize")
 // const PokemonModel = require('./src/models/pokemons')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // const sequelize = new Sequelize(
 //   'pokedex',
