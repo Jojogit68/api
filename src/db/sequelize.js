2dac8fc626f5d01bca142cb7d799e841ebdb1a0f
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-24 18:31:30 +0200

    Migrate from mariadb to postgresql

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -5,10 +5,21 @@ const UserModel = require("../models/users")
 const pokemons = require("./mock-pokemon")
 const bcrypt = require('bcrypt')
 
-const sequelize = new Sequelize(
-	"postgres://pokedex_7sms_user:TYNnZ4YHNvlfyLXy2lhVTWcXFNMgCpNv@dpg-ckrafv9rfc9c73dprvog-a/pokedex_7sms"
-)
+// const sequelize = new Sequelize(
+// 	"postgres://postgres:Postgresql@localhost:5432/pokedex"
+// )
 
+// const sequelize = new Sequelize(
+// 	"postgres://pokedex_7sms_user:TYNnZ4YHNvlfyLXy2lhVTWcXFNMgCpNv@dpg-ckrafv9rfc9c73dprvog-a:5432/pokedex_7sms"
+// )
+
+// const sequelize = new Sequelize(
+// 	"postgres://pokedex_7sms_user:TYNnZ4YHNvlfyLXy2lhVTWcXFNMgCpNv@dpg-ckrafv9rfc9c73dprvog-a/pokedex_7sms"
+// )
+
+const sequelize = new Sequelize(
+	"postgres://pokedex_7sms_user:TYNnZ4YHNvlfyLXy2lhVTWcXFNMgCpNv@dpg-ckrafv9rfc9c73dprvog-a.frankfurt-postgres.render.com:5432/pokedex_7sms?ssl=true"
+)
 
 const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
